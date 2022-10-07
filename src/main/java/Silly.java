115b7077283236e90746249bf143972a7c6a46be
molplu <molly.plunkett@mail.utoronto.ca>
2022-10-07 15:25:11 -0400

    All tests are passing in IntelliJ.

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -78,10 +78,15 @@ public class Silly implements Comparable<Silly>{
      *       Afterwards, set this.name to the concatenation of both
      *       strings (e.g. this.name = [first string] + [second string]).
      *       Make sure you document this method!
+     *
+     *  Creates a new Silly object.
+     *  This constructor takes in two String objects as an argument.
+     *
+     * @param string_1 a string for this Silly instance's name.
+     * @param string_2 a string for this Silly instance's name.
      */
 
-
-
+    public Silly(String string_1, String string_2) {this.name = string_1 + string_2; }
 
 
     public static void main(String[] args) {
@@ -116,7 +121,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +139,7 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -161,6 +167,8 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+
+        return this.name.equals(other.name);
     }
 
     /**
@@ -194,6 +202,12 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+
+        if (this.name.length() < other.name.length()) {return -1; }
+
+        else if (this.name.length() == other.name.length()) {return 0; }
+
+        else {return 1; }
     }
 
     /*
